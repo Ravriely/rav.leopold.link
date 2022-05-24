54e72c3816eecd43a0da54217f9b6994bc5d442a
Ravriely <77391472+Ravriely@users.noreply.github.com>
2022-05-24 09:06:30 +0200

    [+] Added the firsts blocks of the website [+] Stylised the blocks [~] Renamed index.html to index-fr.html

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 /* jshint esversion : 8*/
 
 var info = false;
-var win_name = window.location.pathname.split("/").pop();
+var win_name = window.location.pathname.split('/').pop();
 
-function delay(time) { 
+if (win_name === 'index.html') {
+	window.location.href = 'index-fr.html';
+}
+
+function delay(time) {
 	return new Promise(resolve => setTimeout(resolve, time));
 }
 
 function switchLanguage() {
-	if (win_name == 'index.html') {
+	if (win_name == 'index-fr.html') {
 		window.location.href = 'index-en.html';
 	} else {
-		window.location.href = 'index.html';
+		window.location.href = 'index-fr.html';
 	}
 }
 
@@ -19,7 +23,7 @@ function ToggleView(id) {
 	if (id == 'info') {
 		if (info) {
 			info = false;
-			document.getElementById('info').style.right = '-25%';
+			document.getElementById('info').style.right = '-470px';
 		} else {
 			info = true;
 			document.getElementById('info').style.right = '0%';
